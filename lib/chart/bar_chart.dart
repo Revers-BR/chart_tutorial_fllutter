2b1ccc7e4989bb9f6a8d12b07b75bbf2a5b573dc
Revers-BR <reversrivair@gmail.com>
2025-03-02 07:07:30 -0300

    Adicionado grafico de barra

--- lib/chart/bar_chart.dart
+++ lib/chart/bar_chart.dart
@@ -1,3 +1,5 @@
+import 'package:chart_tutorial/chart_data/bar_chart_data.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class BarChartContent extends StatelessWidget {
@@ -5,6 +7,93 @@ class BarChartContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Bar Chart');
+    return BarChart(
+      BarChartData(
+        maxY: 16,
+        barGroups: barChartGroupData,
+        titlesData: FlTitlesData(
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                switch (value.toInt()) {
+                  case 1:
+                    return Text(
+                      'Mon',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  case 2:
+                    return Text(
+                      'Tues',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  case 3:
+                    return Text(
+                      'Wed',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  case 4:
+                    return Text(
+                      'Thu',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  case 5:
+                    return Text(
+                      'Fri',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  case 6:
+                    return Text(
+                      'Sat',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  case 7:
+                    return Text(
+                      'Sun',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  default:
+                    return Text('');
+                }
+              },
+            ),
+          ),
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(interval: 4, showTitles: true),
+          ),
+        ),
+        borderData: FlBorderData(
+          border: Border.all(color: Colors.white, width: 0.5),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/chart_data/bar_chart_data.dart
@@ -0,0 +1,33 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/services.dart';
+
+List<BarChartGroupData> barChartGroupData = [
+  BarChartGroupData(
+    x: 1,
+    barRods: [BarChartRodData(toY: 10, color: Color(0xff43dde6))],
+  ),
+  BarChartGroupData(
+    x: 2,
+    barRods: [BarChartRodData(toY: 8.6, color: Color(0xff43dde6))],
+  ),
+  BarChartGroupData(
+    x: 3,
+    barRods: [BarChartRodData(toY: 12.6, color: Color(0xff43dde6))],
+  ),
+  BarChartGroupData(
+    x: 4,
+    barRods: [BarChartRodData(toY: 11.4, color: Color(0xff43dde6))],
+  ),
+  BarChartGroupData(
+    x: 5,
+    barRods: [BarChartRodData(toY: 7.5, color: Color(0xff43dde6))],
+  ),
+  BarChartGroupData(
+    x: 6,
+    barRods: [BarChartRodData(toY: 14, color: Color(0xff43dde6))],
+  ),
+  BarChartGroupData(
+    x: 7,
+    barRods: [BarChartRodData(toY: 12.2, color: Color(0xff43dde6))],
+  ),
+];
